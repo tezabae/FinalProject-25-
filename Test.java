c93c4c3ce4a1b6280719752768ed53c719e5e2c2
Agam Darmawan <madridlover085@gmail.com>
2025-10-02 14:16:32 +0700

    singkong

--- Test.java
+++ Test.java
@@ -2,6 +2,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Hi, ini baris kode pertama!(user1)");
         System.out.println("Hi, ini baris kode kedua!(user2)");
+        System.out.println("Hi, ini baris kode ketiga!(user3)");
     }
     
 }
