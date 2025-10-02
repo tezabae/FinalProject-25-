6c8500acd9a034bfdc60143a488c8c28c66abb93
tezabae <tesalonikabenila@gmail.com>
2025-10-02 14:12:11 +0700

    nyoba2

---
+++ Test.java
@@ -0,0 +1,7 @@
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hi, ini baris kode pertama!(user1)");
+        System.out.println("Hi, ini baris kode kedua!(user2)");
+    }
+    
+}
